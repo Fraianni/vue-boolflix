877304d37ee9fd33e0c586689b5851d8b3a83b94
Fraianni <francescoianni1995@gmail.com>
2022-09-28 17:12:18 +0200

    Impostazione esercizio

---
+++ vue-boolflix/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <headerComponent/>
+  </div>
+</template>
+
+<script>
+  import headerComponent from './components/headerComponent.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      headerComponent,
+
+    },
+
+    data() {
+      return{
+        apiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=' + 'ciao'
+      }
+    },
+
+    created(){
+      console.log(this.apiUrl)
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  
+}
+</style>
---
+++ vue-boolflix/src/components/headerComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <input v-model="text_to_search" type="text" placeholder="Cerca film">
+  </header>
+</template>
+
+<script>
+export default {
+    name:'headerComponent'
+}
+</script>
+
+<style scoped>
+    header{
+        height: 15vh;
+        background-color: red;
+    }
+</style>
